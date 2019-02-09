a58507aaa73c535f5e1f353e0d4d7c77a435fc7c
Maximo <maxmonteiro86@gmail.com>
2019-02-09 18:41:16 -0300

    Criada função de inserção no banco

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,18 +1,24 @@
 <?php include('cabecalho.php'); ?>
 <?php
+// criando função de inserção no banco
+function insereProduto($conexao, $nome, $preco)
+{
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    return mysqli_query($conexao, $query);
+}
+
 $nome = $_GET['nome'];
 $preco = $_GET['preco'];
-// Inserindo dados no banco
-// abre conexao
 $conexao = mysqli_connect('localhost', 'root', 'root', 'phploja');
 // query sql
-$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
 // executa query
-if (mysqli_query($conexao, $query)) { ?>
+if (insereProduto($conexao, $nome, $preco)) { ?>
     <p class="alert-success">Produto <?= $nome ?>, <?= $preco ?> adicionado com sucesso!</p>
-<?php } else { ?>
+<?php 
+} else { ?>
     <p class="alert-danger">Produto <?= $nome ?> não foi adicionado!</p>
 <?php 
-} 
+}
 ?>
 <?php include('rodape.php'); ?>
